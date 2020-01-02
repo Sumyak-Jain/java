5dfca940f2ba5ee7625a0ffb583f8e30d9da7c0b
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-02 19:48:01 +0530

    basic java array example

---
+++ java_array.java
@@ -0,0 +1,9 @@
+public class java_array {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        for(int i=0;i<a.length;i++)
+            System.out.println(a[i]);
+        
+    }
+}
+//arrayRefVar=new datatype[size]; (we can also intialize array like this)
