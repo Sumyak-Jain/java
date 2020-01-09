26e5942659bb6ddd99705ee826bb5c974a412256
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-09 23:54:46 +0530

    join method in thread

---
+++ JoinMethod .java
@@ -0,0 +1,22 @@
+class JoinMethod extends Thread{  
+ public void run(){  
+  for(int i=1;i<=10;i++){  
+   try{  
+    Thread.sleep(500);  
+   }catch(Exception e){System.out.println("hee");}  
+  System.out.println(i);  
+  }  
+ }  
+public static void main(String args[]){  
+ TestJoinMethod1 t1=new TestJoinMethod1();  
+ TestJoinMethod1 t2=new TestJoinMethod1();  
+ TestJoinMethod1 t3=new TestJoinMethod1();  
+ t1.start();  
+ try{  
+  t1.join();  
+ }catch(Exception e){System.out.println(e);}  
+  
+ t2.start();  
+ t3.start();  
+ }  
+}  
