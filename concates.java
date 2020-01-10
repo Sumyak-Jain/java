c0e5b8cf3de99003d1f9cf02eaff8cfe77e29d58
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-11 00:21:31 +0530

    solution to immutable string

---
+++ concates.java
@@ -0,0 +1,9 @@
+
+public class concates {
+    public static void main(String[] args) {
+        String s="gaurav";
+        s=s.concat("jain");//s is also reffred to gaurav jain
+        System.out.println(s);
+    }
+    
+}
