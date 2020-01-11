f028ed9d36b9fd864d05f5ce94944fb4751fb2a0
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-12 00:16:34 +0530

    write a string in a file

--- file_output.java
+++ file_output.java
@@ -4,7 +4,9 @@ public class file_output {
     public static void main(String[] args) {
         try{
             FileOutputStream fout= new FileOutputStream("C:\\Users\\GAURAV\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
-            fout.write(65);
+            String s="this is java guide";
+            byte b[]=s.getBytes();
+            fout.write(b);
             fout.close();
             System.out.println("over");
         } catch(IOException e){System.out.println(e);
