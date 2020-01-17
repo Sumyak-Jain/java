92130374ca21d8d192565e96880c640a8cdcea89
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-17 23:29:18 +0530

    bufferedwriter class

---
+++ input_output#8.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class buffered_writer {
+    public static void main(String[] args) throws Exception{
+        FileWriter writerss=new FileWriter("C:\\Users\\GAURAV\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
+        BufferedWriter buff=new BufferedWriter(writerss);
+        buff.write("hello peeps");
+        buff.close();
+        System.out.println("done");
+    }
+    
+}
