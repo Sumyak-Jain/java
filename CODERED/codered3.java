a2a942fc41385ebb66416d043df5e54f9c4c07fd
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-05 11:59:22 +0530

    Update codered3.java

--- CODERED/codered3.java
+++ CODERED/codered3.java
@@ -15,13 +15,13 @@ public class codered3 extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CODERED");
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); 
         jLabel1.setText("please tell us more:");
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); 
         jLabel2.setText("undergoing medicines");
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14));
         jLabel3.setText("any allergies?");
 
         jTextField2.addActionListener(new java.awt.event.ActionListener() {
