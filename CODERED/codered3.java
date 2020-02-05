1a56e603f53882452c130628674156a8b0ff6ced
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-05 12:00:50 +0530

    window 3

--- CODERED/codered3.java
+++ CODERED/codered3.java
@@ -103,9 +103,6 @@ public class codered3 extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(codered3.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new codered3().setVisible(true);
