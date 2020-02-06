fdf1ac8fb03a4bc8630600aacd4620471fdefbd1
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-06 23:28:40 +0530

    Update codered4.java

--- CODERED/codered4.java
+++ CODERED/codered4.java
@@ -100,11 +100,7 @@ public class codered4 extends javax.swing.JFrame {
     }                                        
 
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
