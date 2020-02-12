a3270d985d70d5bdd47f36e5214e1390c721669f
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-12 12:20:12 +0530

    Create case.java

---
+++ case.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Case{
+    public static void main(String args[]){
+        int count=1;
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        char[] ch=new char[line.length()];
+        for(int i=0;i<line.length();i++){
+            ch[i]=line.charAt(i);
+        }
+        for(int i=0;i<line.length();i++){
+            
+             if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                 count++;
+             }
+        }
+        System.out.println(count);
+        
+    }
+}
