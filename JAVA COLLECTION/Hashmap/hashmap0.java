8fa7d7247ca52f46b8d0170596bcf13da90893b2
SUMYAK JAIN <3456gaurav@gmail.com>
2021-03-06 22:37:18 +0530

    Update hashmap0.java

--- JAVA COLLECTION/Hashmap/hashmap0.java
+++ JAVA COLLECTION/Hashmap/hashmap0.java
@@ -1,1 +1,15 @@
+package collection;
+import java.util.*;
+public class hashmap0 {
+    public static void main(String[] args){
+        HashMap <Integer, String> map=new HashMap<Integer, String>();
+        map.put(1,"arsh");
+        map.put(2,"gaurav");
+        map.put(2,"arsh");
+        for(Map.Entry m: map.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
 
+        
+    }
+}
